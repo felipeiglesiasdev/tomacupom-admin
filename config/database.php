7005655e1a316748d65ec7f2a6d9bb00676b8ad0
Felipe Iglesias <felipeiglesiasdev@gmail.com>
2026-03-02 00:58:52 -0300

    /config/database.php

--- config/database.php
+++ config/database.php
@@ -63,6 +63,25 @@ return [
             ]) : [],
         ],
 
+        'mysql_app' => [
+            'driver' => 'mysql',
+            'host' => env('DB_APP_HOST', '127.0.0.1'),
+            'port' => env('DB_APP_PORT', '3306'),
+            'database' => env('DB_APP_DATABASE', 'tomacupom_app'), // Banco de dados da aplicação
+            'username' => env('DB_APP_USERNAME', 'root'),
+            'password' => env('DB_APP_PASSWORD', ''),
+            'unix_socket' => env('DB_APP_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'mariadb' => [
             'driver' => 'mariadb',
             'url' => env('DB_URL'),
